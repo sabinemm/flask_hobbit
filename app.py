2d826fa38bcb38284afaee692f0e6544cd9e9a0e
Sabine <sabine@21.lv>
2020-05-07 20:49:50 +0200

    template

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+import os
+
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello, Flask!"
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            debug=True)
